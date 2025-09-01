b3d48726818ba49958e2f6503a4b496e16fb27f7
CarloMart88 <carlo.martino1988@gmail.com>
2025-09-01 17:36:07 +0200

    feat: define useState

--- components/Main.jsx
+++ components/Main.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import { useState, useEffect } from "react";
 
 //Create un nuovo progetto React e implementate un sistema di filtro per una lista di film in base al genere.
 
@@ -11,8 +11,43 @@ const initialFilms = [
   { title: "Pulp Fiction", genre: "Thriller" },
 ];
 
+const [genre, setGenres] = useState("");
+const [filterdFilms, setfilterdFilms] = useState(initialFilms);
+
+useEffect(() => {
+  console.log("ok");
+}, []);
+
+//Il filtro deve funzionare dinamicamente quando l'utente seleziona un genere dalla select.
+//Se non viene selezionato alcun genere, devono essere mostrati tutti i film.
+
 function Main() {
-  return <div>ciao</div>;
+  return (
+    <div className="container">
+      <div className="row">
+        <div className="col-12">
+          <select>
+            {initialFilms.map((film) => {
+              const { title, genre } = film;
+              return (
+                <>
+                  <option>{genre}</option>
+                </>
+              );
+            })}
+          </select>
+        </div>
+        {initialFilms.map((film) => {
+          const { title, genre } = film;
+          return (
+            <>
+              <div className="col-5">{title}</div>
+            </>
+          );
+        })}
+      </div>
+    </div>
+  );
 }
 
 export default Main;
